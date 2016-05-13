c4c7ff1546c3a0a0aa25c03d44734de40f9e78f3
KimGwangok <naedo5714@gmail.com>
2016-05-13 17:44:33 +0900

    2016.05.13

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var express = require('express');
+var path = require('path');
 
 var app = express();
 
-app.get('/', function(req,res){
-  res.send('hello world');
-});
+app.use(express.static(path.join(__dirname, 'public')));
+//console.log(__dirname + '/public')
 
-app.listen(3000,function(){
-  console.log('server on');
+app.listen(3030,function(){
+  console.log('Fucking sever on!');
 });
